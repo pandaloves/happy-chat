0f9f7dea2c16a791a95cf1ba4ca652fe5e933738
meiyi <meiyinghu18@gmail.com>
2024-08-13 01:54:06 +0200

    new

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,15 @@ export default defineConfig({
   plugins: [react(), sentryVitePlugin({
     org: "jensen-fw",
     project: "happy-chat"
+  }), sentryVitePlugin({
+    org: "jensen-fq",
+    project: "happy-chat"
+  }), sentryVitePlugin({
+    org: "school-0fs",
+    project: "happy-chat"
+  }), sentryVitePlugin({
+    org: "jensen-yrkeshogskola-13",
+    project: "javascript-react"
   })],
 
   build: {
