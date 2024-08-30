d0a19a3a3fc91b0f9b0f3241e76cfaa6582a58e0
meiyi <meiyinghu18@gmail.com>
2024-08-30 15:56:20 +0200

    new

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,7 @@
 import { useContext, useState, useEffect } from "react";
 import { UserContext } from "../context/UserContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { ToastContainer } from "react-toastify";
 import { ImSpinner6 } from "react-icons/im";
 import { IoNavigate } from "react-icons/io5";
@@ -53,7 +55,6 @@ const Register = () => {
 
   const handleAvatarClick = (e) => {
     e.preventDefault();
-
     const randomAvatar = `https://i.pravatar.cc/150?img=${
       Math.floor(Math.random() * 70) + 1
     }`;
@@ -94,57 +95,48 @@ const Register = () => {
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-
               <div className="relative">
                 <input
-                  placeholder="password"
+                  placeholder="Password"
                   className="input input-bordered w-full"
                   required
                   type={showPassword ? "text" : "password"}
                   onChange={(e) => setPassword(e.target.value)}
                   value={cleanData(password)}
                 />
-
                 <button
+                  type="button"
                   className="btn btn-outline absolute inset-y-0 right-0 flex items-center"
                   onClick={() => setShowPassword(!showPassword)}
                 >
-                  {showPassword ? (
-                    <i className="fas fa-eye-slash fa-2x"></i>
-                  ) : (
-                    <i className="fas fa-eye fa-2x"></i>
-                  )}
+                  <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
                 </button>
               </div>
             </div>
 
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Confirm password</span>
+                <span className="label-text">Confirm Password</span>
               </label>
-
               <div className="relative">
                 <input
-                  placeholder="Confirm password"
+                  placeholder="Confirm Password"
                   className="input input-bordered w-full"
                   required
                   type={showConfirmPassword ? "text" : "password"}
                   onChange={handleConfirmPasswordChange}
                   value={cleanData(confirmPassword)}
                 />
-
                 <button
+                  type="button"
                   className="btn btn-outline absolute inset-y-0 right-0 flex items-center"
                   onClick={() => setShowConfirmPassword(!showConfirmPassword)}
                 >
-                  {showConfirmPassword ? (
-                    <i className="fas fa-eye-slash fa-2x"></i>
-                  ) : (
-                    <i className="fas fa-eye fa-2x"></i>
-                  )}
+                  <FontAwesomeIcon
+                    icon={showConfirmPassword ? faEye : faEyeSlash}
+                  />
                 </button>
               </div>
-
               {!passwordMatch && (
                 <span className="text-red-500 text-sm mt-1 self-end">
                   * Passwords do not match.
@@ -168,11 +160,11 @@ const Register = () => {
 
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Confirm email</span>
+                <span className="label-text">Confirm Email</span>
               </label>
               <input
                 type="text"
-                placeholder="Confirm email"
+                placeholder="Confirm Email"
                 className="input input-bordered w-full"
                 required
                 onChange={handleConfirmEmailChange}
@@ -189,7 +181,6 @@ const Register = () => {
               <label htmlFor="avatar" className="text-base">
                 Avatar
               </label>
-
               <div className="avatar">
                 <div className="w-24 rounded-xl">
                   <img
@@ -203,17 +194,20 @@ const Register = () => {
                 onClick={handleAvatarClick}
                 className="btn btn-outline btn-secondary text-base transition ease-in-out delay-150 hover:-translate-v-1 hover:scale-110"
               >
-                Click and choose
+                Click and Choose
               </button>
             </div>
 
             <div className="form-control mt-6">
-              <button className="btn btn-outline btn-primary text-base transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
+              <button
+                type="submit"
+                className="btn btn-outline btn-primary text-base transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
+              >
                 {isLoading ? (
                   <ImSpinner6 className="animate-spin text-2xl mr-2" />
                 ) : (
                   <>
-                    Sign up
+                    Sign Up
                     <IoNavigate className="ml-2" />
                   </>
                 )}
