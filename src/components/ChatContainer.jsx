295f065a967d583f61f583c9a4a36dc77ee0b752
meiyi <meiyinghu18@gmail.com>
2024-09-02 19:03:37 +0200

    new

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -2,13 +2,13 @@ import { useContext, useEffect, useState, useRef } from "react";
 import { UserContext } from "../context/UserContext";
 import NoAvatar from "../assets/img/NoAvatar.png";
 import { ChatContext } from "../context/ChatContext";
-import { v4 as uuidv4 } from "uuid";
 import { format } from "timeago.js";
 import { ImSpinner6 } from "react-icons/im";
 
 const ChatContainer = () => {
   const [deletingMessageId, setDeletingMessageId] = useState(null);
   const [prevMessagesLength, setPrevMessagesLength] = useState(0);
+
   const scrollRef = useRef();
 
   const { authUser } = useContext(UserContext);
@@ -21,11 +21,6 @@ const ChatContainer = () => {
     fetchMessages,
     deleteMessage,
   } = useContext(ChatContext);
-
-  const [imgSrc, setImgSrc] = useState(invitedAvatar || NoAvatar);
-  const handleImageError = () => {
-    setImgSrc(NoAvatar);
-  };
 
   useEffect(() => {
     if (conversationId) {
@@ -59,7 +54,7 @@ const ChatContainer = () => {
           {messages &&
             messages.map((message, index) => (
               <div
-                key={uuidv4()}
+                key={message.id}
                 className={`chat ${
                   message.userId === authUser.id ? "chat-end" : "chat-start"
                 }`}
@@ -71,10 +66,9 @@ const ChatContainer = () => {
                       alt="avatar"
                       src={
                         message.userId === authUser.id
-                          ? authUser.avatar
-                          : imgSrc
+                          ? authUser.avatar || NoAvatar
+                          : invitedAvatar || NoAvatar
                       }
-                      onError={handleImageError}
                     />
                   </div>
                 </div>
